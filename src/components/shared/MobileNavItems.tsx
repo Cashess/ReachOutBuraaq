4f841b6dfe95f8f434f32bc92a3ff21a680ca92e
Aminu sulaiman yusuf <aminugotie42@gmail.com>
2024-04-09 12:35:39 +0300

    user schema update

--- src/components/shared/MobileNavItems.tsx
+++ src/components/shared/MobileNavItems.tsx
@@ -7,9 +7,9 @@ const MobileNavItems = () => {
     <nav className="md:hidden text-black">
         <Sheet>
         <SheetTrigger className="align-middle">
-            <Image src="/icons/menu.svg" alt="menu" width={23} height={23} className="cursor-pointer" />
+            <Image src="/icons/menu.svg" alt="menu" width={23} height={23} className="cursor-pointer"/>
         </SheetTrigger>
-        <SheetContent className="flex flex-col gap-5 bg-green-300 md:hidden">
+        <SheetContent className="flex flex-col gap-5 bg-gray-400 md:hidden">
             <Image src="/buraaq Logo.png" alt="buraaq" width={125} height={70} className="rounded-full"/>
             <Separator className="border border-red-300" />
         <NavbarItems/>
--- src/lib/database/models/user.model.ts
+++ src/lib/database/models/user.model.ts
@@ -1,46 +1,40 @@
 import { Schema, model, models } from "mongoose";
 
-
 const UserSchema = new Schema({
     clerkId:{
         type:String, 
         required:true,
         unique:true,
-
     },
     email:{
         type:String, 
         required:true,
         unique:true,
-        
     },
     username:{
         type:String, 
         required:true,
         unique:true,
-        
     },
     firstName:{
         type:String, 
         required:true,
-        
-        
     },
     lastName:{
         type:String, 
         required:true,
-        
-        
     },
     photo:{
         type:String, 
         required:true,
-        
-        
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true  // Ensures password is unique
     }
-})
+});
 
-const User = models.User || model("User", UserSchema)
+const User = models.User || model("User", UserSchema);
 
-export default User
-
+export default User;
